5375313771f6e8451d5e08d06641698b8a530e22
prafful <prraful@gmail.com>
2020-06-17 03:21:33 -0700

    day 03 - tuple, dictionary, string, loop, condition

--- 007 python_tuple.py
+++ 007 python_tuple.py
@@ -4,3 +4,17 @@ print(type(my_tuple1))
 print(type(my_tuple2))
 my_tuple3 = 22, 44, 88
 print(type(my_tuple3))
+scores = [11, 22, 44]
+checktuple  = tuple(scores)
+print(checktuple)
+#onlyone  = tuple(21)
+onlyone = (21, )
+print(onlyone)
+#TypeError: 'tuple' object does not support item assignment
+print(checktuple[0])
+#checktuple[0]=12
+addmetuple = [22,32, 442]
+newtuple  =(5, 8, addmetuple)
+print(newtuple)
+for i in newtuple:
+    print(i)
